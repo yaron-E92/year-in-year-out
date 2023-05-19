8f707a0d8203785deac0ecb2bcd886d50ef0f210
Yaron Efrat <yaronefrat92@gmail.com>
2023-05-19 12:30:42 +0200

    [YIYO-16]: Create IApplicationDbContext and IDbEntity
    In this commit, we create the abstractions for the db context and the db entity to be used by the queries.
    In the db context we have the connection and `DbSet`s for the different db entities (which mirror the domain
    entities). For that we already need EntityFramework

---
+++ src/YaronEfrat.Yiyo.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace YaronEfrat.Yiyo.Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    IDbConnection Connection { get; }
+
+    DatabaseFacade Database { get; }
+
+    DbSet<FeelingEntity> Feelings { get; }
+
+    bool HasChanges { get; }
+
+    DbSet<MottoEntity> Mottos { get; }
+
+    DbSet<PersonalEventEntity> PersonalEvents { get; }
+
+    DbSet<WorldEventEntity> WorldEvents { get; }
+
+    DbSet<YearInEntity> YearIns { get; }
+
+    DbSet<YearOutEntity> YearOuts { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/YaronEfrat.Yiyo.Application/Interfaces/IDbEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace YaronEfrat.Yiyo.Application.Interfaces;
+internal interface IDbEntity
+{
+    int ID { get; }
+}
