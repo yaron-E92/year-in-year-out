7b92ce9f21433fc51d85070b5b892d0b68b1dbcf
Yaron Efrat <yaronefrat92@gmail.com>
2023-07-21 12:04:58 +0200

    [YIYO-16]: Create GetMottoQuery
    In this commit, we create the `GetMottoQuery` and its handler.
    They are implementing MediatR interfaces.

---
+++ src/YaronEfrat.Yiyo.Application/Queries/GetMottoQuery.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+using YaronEfrat.Yiyo.Application.Models;
+
+namespace YaronEfrat.Yiyo.Application.Queries;
+
+public class GetMottoQuery : IRequest<MottoEntity>
+{
+    public int Id { get; init; }
+}
+
+public class GetMottoQueryHandler : IRequestHandler<GetMottoQuery, MottoEntity>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetMottoQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<MottoEntity> Handle(GetMottoQuery request, CancellationToken cancellationToken = default)
+    {
+        return _context.Mottos.SingleOrDefault(motto => motto.ID.Equals(request.Id))!;
+    }
+}
