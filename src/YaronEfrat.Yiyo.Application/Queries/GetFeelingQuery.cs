cf3174b7afc79efcc86060795825aaa762ee6f97
Yaron Efrat <yaronefrat92@gmail.com>
2023-07-21 11:38:41 +0200

    [YIYO-16]: Implement title based query in GetFeelingQuery
    In this commit we add Title to the `GetFeelingQuery` and we expand the `Handle` method
    in `GetFeelingQueryHandler` to handle both id and title

--- src/YaronEfrat.Yiyo.Application/Queries/GetFeelingQuery.cs
+++ src/YaronEfrat.Yiyo.Application/Queries/GetFeelingQuery.cs
@@ -8,6 +8,8 @@ namespace YaronEfrat.Yiyo.Application.Queries;
 public class GetFeelingQuery : IRequest<FeelingEntity>
 {
     public int Id { get; init; }
+
+    public string Title { get; init; }
 }
 
 public class GetFeelingQueryHandler : IRequestHandler<GetFeelingQuery, FeelingEntity>
@@ -21,6 +23,13 @@ public class GetFeelingQueryHandler : IRequestHandler<GetFeelingQuery, FeelingEn
 
     public async Task<FeelingEntity> Handle(GetFeelingQuery request, CancellationToken cancellationToken = default)
     {
-        return _context.Feelings.SingleOrDefault(feel => feel.ID.Equals(request.Id));
+        if (request.Id > 0)
+        {
+            return _context.Feelings.SingleOrDefault(feel => feel.ID.Equals(request.Id))!;
+        }
+
+        return (!string.IsNullOrWhiteSpace(request.Title)
+            ? _context.Feelings.SingleOrDefault(feel => feel.Title.Equals(request.Title))
+            : null)!;
     }
 }
