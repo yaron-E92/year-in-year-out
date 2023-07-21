2e644ea2035af2bf6b3f49603f38b98fee02bfa0
Yaron Efrat <yaronefrat92@gmail.com>
2023-07-21 13:29:55 +0200

    [YIYO-16]: Create GetPersonalEventQuery
    In this commit, we create the `GetPersonalEventQuery` and its handler.
    They are implementing MediatR interfaces.

---
+++ src/YaronEfrat.Yiyo.Application/Queries/GetPersonalEventQuery.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+using YaronEfrat.Yiyo.Application.Models;
+
+namespace YaronEfrat.Yiyo.Application.Queries;
+
+public class GetPersonalEventQuery : IRequest<PersonalEventEntity>
+{
+    public int Id { get; init; }
+
+    public string Title { get; init; }
+}
+
+public class GetPersonalEventQueryHandler : IRequestHandler<GetPersonalEventQuery, PersonalEventEntity>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetPersonalEventQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PersonalEventEntity> Handle(GetPersonalEventQuery request, CancellationToken cancellationToken = default)
+    {
+        if (request.Id > 0)
+        {
+            return _context.PersonalEvents.SingleOrDefault(pe => pe.ID.Equals(request.Id))!;
+        }
+
+        return (!string.IsNullOrWhiteSpace(request.Title)
+            ? _context.PersonalEvents.SingleOrDefault(pe => pe.Title.Equals(request.Title))
+            : null)!;
+    }
+}
