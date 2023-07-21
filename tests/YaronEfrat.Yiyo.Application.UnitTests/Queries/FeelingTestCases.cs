dc643ed0d7376b5a8d1fa70583433221d874e872
Yaron Efrat <yaronefrat92@gmail.com>
2023-07-21 11:18:09 +0200

    [YIYO-16]: Create unit tests for GetFeelingQuery
    In this commit, we prep the unit testing of `GetFeelingQuery`'s handler.
    We prepped the query itself with an Id, and an internal handler having an `IApplicationDbContext`
    with the Handle not implemented.
    We then create the test suite with a mock of `IApplicationDbContext`, and the handler.
    We also need to create a test case source that will supply the mock rows of feelings to the
    mock at setup and then sends the correct expected stuff for the test cases.
    We will allow several options in the query, but I am not sure whether this is the correct approach. I will ask for
    feedback from people.
    
    We also add FluentAssertions to the unit test project.
    We make the test case source with `FeelingEntity`, and also everything related to the query.

---
+++ tests/YaronEfrat.Yiyo.Application.UnitTests/Queries/FeelingTestCases.cs
@@ -0,0 +1,19 @@
+﻿using YaronEfrat.Yiyo.Application.Models;
+
+namespace YaronEfrat.Yiyo.Application.UnitTests.Queries;
+internal class FeelingTestCases
+{
+    internal static readonly IReadOnlyList<FeelingEntity> Feelings = new List<FeelingEntity>()
+    {
+        new()
+        {
+            ID = 1,
+            Title = "Sad",
+        },
+        new()
+        {
+            ID = 2,
+            Title = "Happy",
+        },
+    };
+}
---
+++ tests/YaronEfrat.Yiyo.Application.UnitTests/Queries/GetFeelingQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore;
+
+using Moq;
+
+using NUnit.Framework;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+using YaronEfrat.Yiyo.Application.Models;
+using YaronEfrat.Yiyo.Application.Queries;
+
+namespace YaronEfrat.Yiyo.Application.UnitTests.Queries;
+
+internal class GetFeelingQueryHandlerTests
+{
+    private Mock<IApplicationDbContext> _dbContextMock;
+
+    private GetFeelingQueryHandler _getFeelingQueryHandler;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dbContextMock = new Mock<IApplicationDbContext>();
+
+        var queryable =  FeelingTestCases.Feelings.AsQueryable();
+        var dbSetMock = new Mock<DbSet<FeelingEntity>>();
+        dbSetMock.As<IQueryable<FeelingEntity>>().Setup(m => m.Provider).Returns(queryable.Provider);
+        dbSetMock.As<IQueryable<FeelingEntity>>().Setup(m => m.Expression).Returns(queryable.Expression);
+        dbSetMock.As<IQueryable<FeelingEntity>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+        dbSetMock.As<IQueryable<FeelingEntity>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+        _dbContextMock.Setup(mock => mock.Feelings)
+            .Returns(dbSetMock.Object);
+        _getFeelingQueryHandler = new GetFeelingQueryHandler(_dbContextMock.Object);
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    public async Task Should_ReturnCorrectFeeling_When_SearchingForExistingId(int id)
+    {
+        // Act
+        FeelingEntity feeling = await _getFeelingQueryHandler.Handle(new GetFeelingQuery {Id = id});
+
+        // Assert
+        feeling.ID.Should().Be(id);
+    }
+
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(100000)]
+    public async Task Should_ReturnNull_When_SearchingForNonExistantId(int id)
+    {
+        // Act
+        FeelingEntity feeling = await _getFeelingQueryHandler.Handle(new GetFeelingQuery { Id = id });
+
+        // Assert
+        feeling.Should().BeNull();
+    }
+}
