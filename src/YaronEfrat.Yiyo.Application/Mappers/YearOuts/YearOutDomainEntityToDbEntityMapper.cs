c462eda03efd93a4ea2552ad7ecb2e0cac45ad76
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-14 16:23:37 +0200

    [YIYO-18] feat(mappers): Add domain to db mappers for year in/out

---
+++ src/YaronEfrat.Yiyo.Application/Mappers/YearOuts/YearOutDomainEntityToDbEntityMapper.cs
@@ -0,0 +1,28 @@
+﻿using YaronEfrat.Yiyo.Application.Interfaces;
+using YaronEfrat.Yiyo.Application.Models;
+using YaronEfrat.Yiyo.Domain.Reflection.Models.Entities;
+
+namespace YaronEfrat.Yiyo.Application.Mappers.YearOuts;
+
+public class YearOutDomainEntityToDbEntityMapper : IDomainEntityToDbEntityMapper<YearOut, YearOutEntity>
+{
+    private readonly IDomainEntityToDbEntityMapper<Feeling, FeelingEntity> _feelingMapper;
+    private readonly IDomainEntityToDbEntityMapper<Motto, MottoEntity> _mottoMapper;
+    private readonly IDomainEntityToDbEntityMapper<PersonalEvent, PersonalEventEntity> _personalEventMapper;
+
+    public YearOutDomainEntityToDbEntityMapper(IDomainEntityToDbEntityMapper<Feeling, FeelingEntity> feelingMapper,
+        IDomainEntityToDbEntityMapper<Motto, MottoEntity> mottoMapper,
+        IDomainEntityToDbEntityMapper<PersonalEvent, PersonalEventEntity> personalEventMapper)
+    {
+        _feelingMapper = feelingMapper;
+        _mottoMapper = mottoMapper;
+        _personalEventMapper = personalEventMapper;
+    }
+
+    public void Map(YearOut domainEntity, YearOutEntity existingDbEntity)
+    {
+        _feelingMapper.MapMany(domainEntity.Feelings, existingDbEntity.Feelings);
+        _mottoMapper.Map(domainEntity.Motto, existingDbEntity.Motto);
+        _personalEventMapper.MapMany(domainEntity.PersonalEvents, existingDbEntity.PersonalEvents);
+    }
+}
