6b6f2ed4c082ed22f35c921d567971a7c3c42d38
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-16 12:26:24 +0200

    [YIYO-18] refactor: Create request validation method
    In this commit, we create extension methods for MediatR's `IRequest` class.
    The most important one is one that uses reflection to retrieve the
    `IDbEntity` based properties on commands. With that, you can have the uniform
    validation methods like `IsValidAddCommand` (which we create here).

---
+++ src/YaronEfrat.Yiyo.Application/RequestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using MediatR;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+
+namespace YaronEfrat.Yiyo.Application;
+internal static class RequestExtensions
+{
+    public static T? GetRequestContent<T>(this IRequest<T> request) where T : class, IDbEntity
+    {
+        T? content = request.GetType().GetRuntimeProperties().Where(
+            info => info.PropertyType == typeof(T)).ToArray()[0].GetValue(request) as T;
+        return content;
+    }
+
+    public static bool IsValidAddCommand<T>(this IRequest<T> request) where T : class, IDbEntity
+    {
+        return request != null! && request.GetRequestContent() is {ID: 0};
+    }
+
+
+}
