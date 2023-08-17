bed37abc4a7674b841c90f718c4b542c81d8e3a0
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-17 18:54:20 +0200

    [29] feat: Add basic persistence later DI
    In this commit, we add some basic persistence layer DI:
    We inject the new `ApplicationDbContext` as a `DbContext` (with connection string from the configuration) and in place of `IApplicationDbContext`

---
+++ src/YaronEfrat.Yiyo.Persistence/PersistenceLayerDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+
+namespace YaronEfrat.Yiyo.Persistence;
+
+public static class PersistenceLayerDependencyInjection
+{
+    private const string DefaultConnection = "DefaultConnection";
+
+    public static void AddPersistenceLayerDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString(DefaultConnection)));
+
+        services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetService<ApplicationDbContext>() ?? throw new Exception("Could not get DB context."));
+    }
+}
