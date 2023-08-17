dfc8864ab0d6650885cdd68685672e333e9545d1
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-17 20:01:54 +0200

    [12] feat: Add controllers for YearIn and YearOut

---
+++ src/YaronEfrat.Yiyo.WebApi/Controllers/YearInController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace YaronEfrat.Yiyo.WebApi.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class YearInController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<YearInController> _logger;
+
+    public YearInController(IMediator mediator, ILogger<YearInController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+}
---
+++ src/YaronEfrat.Yiyo.WebApi/Controllers/YearOutController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace YaronEfrat.Yiyo.WebApi.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class YearOutController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<YearOutController> _logger;
+
+    public YearOutController(IMediator mediator, ILogger<YearOutController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+}
