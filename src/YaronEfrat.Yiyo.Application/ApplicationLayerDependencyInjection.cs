5a9ecff3f84060925748de125d2a8ee19527a5f2
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-17 13:12:19 +0200

    [20] feat: Add Application layer DI

---
+++ src/YaronEfrat.Yiyo.Application/ApplicationLayerDependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using YaronEfrat.Yiyo.Application.Interfaces;
+using YaronEfrat.Yiyo.Application.Mappers.Feelings;
+using YaronEfrat.Yiyo.Application.Mappers.Mottos;
+using YaronEfrat.Yiyo.Application.Mappers.PersonalEvents;
+using YaronEfrat.Yiyo.Application.Mappers.WorldEvents;
+using YaronEfrat.Yiyo.Application.Mappers.YearIns;
+using YaronEfrat.Yiyo.Application.Mappers.YearOuts;
+using YaronEfrat.Yiyo.Application.Models;
+using YaronEfrat.Yiyo.Domain.Reflection.Models.Entities;
+
+namespace YaronEfrat.Yiyo.Application;
+
+public static class ApplicationLayerDependencyInjection
+{
+    public static void AddApplicationLayerDependencyInjection(this IServiceCollection services)
+    {
+        // Db to domain mappers
+        services
+            .AddScoped<IDbEntityToDomainEntityMapper<FeelingEntity, Feeling>,
+                FeelingDbEntityToDomainEntityMapper>()
+            .AddScoped<IDbEntityToDomainEntityMapper<MottoEntity, Motto>,
+                MottoDbEntityToDomainEntityMapper>()
+            .AddScoped<IDbEntityToDomainEntityMapper<PersonalEventEntity, PersonalEvent>,
+                PersonalEventDbEntityToDomainEntityMapper>()
+            .AddScoped<IDbEntityToDomainEntityMapper<WorldEventEntity, WorldEvent>,
+                WorldEventDbEntityToDomainEntityMapper>()
+            .AddScoped<IDbEntityToDomainEntityMapper<YearInEntity, YearIn>,
+                YearInDbEntityToDomainEntityMapper>()
+            .AddScoped<IDbEntityToDomainEntityMapper<YearOutEntity, YearOut>,
+                YearOutDbEntityToDomainEntityMapper>();
+
+        // Domain to db mappers
+        services
+            .AddScoped<IDomainEntityToDbEntityMapper<Feeling, FeelingEntity>,
+                FeelingDomainEntityToDbEntityMapper>()
+            .AddScoped<IDomainEntityToDbEntityMapper<Motto, MottoEntity>,
+                MottoDomainEntityToDbEntityMapper>()
+            .AddScoped<IDomainEntityToDbEntityMapper<PersonalEvent, PersonalEventEntity>,
+                PersonalEventDomainEntityToDbEntityMapper>()
+            .AddScoped<IDomainEntityToDbEntityMapper<WorldEvent, WorldEventEntity>,
+                WorldEventDomainEntityToDbEntityMapper>()
+            .AddScoped<IDomainEntityToDbEntityMapper<YearIn, YearInEntity>,
+                YearInDomainEntityToDbEntityMapper>()
+            .AddScoped<IDomainEntityToDbEntityMapper<YearOut, YearOutEntity>,
+                YearOutDomainEntityToDbEntityMapper>();
+
+        // Mediatr
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationLayerDependencyInjection).Assembly));
+    }
+}
