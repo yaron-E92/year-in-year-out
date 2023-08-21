23a41e4cef00cdacf7cd3301b522a513fac55734
Yaron Efrat <yaronefrat92@gmail.com>
2023-08-21 16:05:47 +0200

    [14]: Create extension methods for ControllerBase controller route
    As part of the Created response I want to return the URI as REST would
    want me to. For that I need the URL of the controller. FULL URL.
    As I explore my options, I realize I will need it in all controllers.
    I briefly flirt with the idea of having a superclass, but I then realize
    that it is smarter to just to an extension method to ControllerBase, since
    all my controllers already inherit from it, and having a class in between
    just for a couple of utility methods is not necessary.
    So I create extension methods to retrieve the controller route.

---
+++ src/YaronEfrat.Yiyo.WebApi/Controllers/ControllerBaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YaronEfrat.Yiyo.WebApi.Controllers;
+
+public static class ControllerBaseExtensions
+{
+    internal static string ControllerRoute(this ControllerBase contoller) =>
+        $"https://{contoller.CurrentHost()}/api/{contoller.ControllerName()}";
+
+    private static string ControllerName(this ControllerBase contoller) =>
+        contoller.HttpContext.GetRouteData().Values["controller"]?.ToString()!;
+
+    private static string CurrentHost(this ControllerBase contoller) =>
+        contoller.HttpContext.Request.Host.ToString();
+}
